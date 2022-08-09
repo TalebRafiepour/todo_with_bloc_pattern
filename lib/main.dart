55ccf87b0dede238abda54ad8b3c78d36a40c519
BehradX <31148015+behradkhadem@users.noreply.github.com>
2022-08-09 10:12:51 +0430

    ✨ Added Hydrated Bloc Package
    added path provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:todo_with_bloc_pattern/foundation/constants/routes.dart';
 import 'package:todo_with_bloc_pattern/foundation/router.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'foundation/bloc_observer.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final hydratedStorage = await HydratedStorage.build(
+    storageDirectory: await getApplicationDocumentsDirectory(),
+  );
+  HydratedBlocOverrides.runZoned(
+    () {
+      runApp(const MyApp());
+    },
+    storage: hydratedStorage,
+    blocObserver: ToDoBlocObserver(),
+  );
 }
 
 class MyApp extends StatelessWidget {
