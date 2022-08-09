861234ed4b96c8117f6e9ab801a9b7c2668524b1
BehradX <31148015+behradkhadem@users.noreply.github.com>
2022-08-09 11:56:06 +0430

    ➕ Added No Task Indicator

--- lib/todo/view/home/widgets/task_listview_widget.dart
+++ lib/todo/view/home/widgets/task_listview_widget.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../bloc/todo/todo_bloc.dart';
 import '../../../bloc/todo/todo_state.dart';
+import 'no_task_indicator_widget.dart';
 import 'task_item_widget.dart';
 
 class TaskListViewWidget extends StatelessWidget {
@@ -12,14 +13,19 @@ class TaskListViewWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<TodoBloc, TodoState>(builder: (context, state) {
-      return ListView.builder(
-          shrinkWrap: true,
-          physics: const NeverScrollableScrollPhysics(),
-          itemCount: state.tasks.length,
-          itemBuilder: (context, index) {
-            return TaskItemWidget(task: state.tasks[index]);
-          });
-    });
+    return BlocBuilder<TodoBloc, TodoState>(
+      builder: (context, state) {
+        if (state.tasks.isEmpty) {
+          return const NoTaskIndicatorWidget();
+        }
+        return ListView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: state.tasks.length,
+            itemBuilder: (context, index) {
+              return TaskItemWidget(task: state.tasks[index]);
+            });
+      },
+    );
   }
 }
